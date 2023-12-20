568783d6a6e77349ffd6884515483a8c58dd3cb9
Ayo <Ayo.meshe@gmail.com>
2023-12-20 15:25:34 +0000

    implementing log & register components

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,3 +1,10 @@
 export default function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <form className="register">
+      <h1>Register</h1>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Register</button>
+    </form>
+  );
 }
