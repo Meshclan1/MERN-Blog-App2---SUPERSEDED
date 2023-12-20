dcfeede4f1bdbb254d1d9c3f3cef67383002b203
Ayo <Ayo.meshe@gmail.com>
2023-12-20 14:09:57 +0000

    building homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,83 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>Testing Changes 1,2,3...</div>
-    </>
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://lp-cms-production.imgix.net/2023-08/-CantoiStock-975466162-RFC.jpg"
+            alt="Brazil"
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Brazil Trip Idea</h2>
+          <p className="info">
+            <a className="author">Ayo M.</a>
+            <time>2023-11-23 13:59</time>
+          </p>
+          <p className="summary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+            numquam illum quasi ad asperiores, laboriosam sapiente praesentium?
+            Harum debitis ea voluptas eveniet inventore sapiente animi,
+            excepturi illo consectetur quod natus!
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://lp-cms-production.imgix.net/2023-08/-CantoiStock-975466162-RFC.jpg"
+            alt="Brazil"
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Brazil Trip Idea</h2>
+          <p className="info">
+            <a className="author">Ayo M.</a>
+            <time>2023-11-23 13:59</time>
+          </p>
+          <p className="summary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+            numquam illum quasi ad asperiores, laboriosam sapiente praesentium?
+            Harum debitis ea voluptas eveniet inventore sapiente animi,
+            excepturi illo consectetur quod natus!
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://lp-cms-production.imgix.net/2023-08/-CantoiStock-975466162-RFC.jpg"
+            alt="Brazil"
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Brazil Trip Idea</h2>
+          <p className="info">
+            <a className="author">Ayo M.</a>
+            <time>2023-11-23 13:59</time>
+          </p>
+          <p className="summary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+            numquam illum quasi ad asperiores, laboriosam sapiente praesentium?
+            Harum debitis ea voluptas eveniet inventore sapiente animi,
+            excepturi illo consectetur quod natus!
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
